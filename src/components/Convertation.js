2366deae1538ac6bd96d30970812f6aae56c8082
Jekeee <glooffee@gmail.com>
2022-10-21 22:32:15 +0300

    patch-set-timout

--- src/components/Convertation.js
+++ src/components/Convertation.js
@@ -1,81 +1,77 @@
 import React, { useState } from "react";
-import { convertExchange } from '../api'
+import { convertExchange } from "../api";
 
-
+let timeout = null;
 
 function Convertation({ currencyRate }) {
-  const [firstInputValue, setFirstInputValue] = useState('0');
-  const [firstCurrency, setFirstCurrency] = useState('USD')
-  const [secondInputValue, setSecondInputValue] = useState('0');
-  const [secondCurrency, setSecondCurrency] = useState('USD')
-  
-  let timeout = null;
+  const [firstInputValue, setFirstInputValue] = useState("0");
+  const [firstCurrency, setFirstCurrency] = useState("USD");
+  const [secondInputValue, setSecondInputValue] = useState("0");
+  const [secondCurrency, setSecondCurrency] = useState("USD");
 
   const handleFirstInputChange = (e) => {
     clearTimeout(timeout);
-    const { value } = e.target
+    const { value } = e.target;
     setFirstInputValue(value);
-    
-     timeout = setTimeout(() => (
-        
+    timeout = setTimeout(
+      () => {
         convertExchange(firstCurrency, secondCurrency, value)
-        .then((data) => {
+          .then((data) => {
             const result = data.result;
             setSecondInputValue(result.toFixed(2));
-        })
-        .catch((error) => console.log("error", error))
-    
-     ), 3000);
+          })
+          .catch((error) => console.log("error", error));
+      },500);
   };
 
-  
-
   const handleFirstSelectChange = (e) => {
-    const { value } = e.target
+    const { value } = e.target;
     setFirstCurrency(value);
-
     convertExchange(value, secondCurrency, firstInputValue)
       .then((data) => {
         const result = data.result;
         setSecondInputValue(result.toFixed(2));
       })
       .catch((error) => console.log("error", error));
-  }
+  };
 
   const handleSecondInputValue = (e) => {
-    const { value } = e.target
+    clearTimeout(timeout);
+    const { value } = e.target;
     setSecondInputValue(value);
-
-    convertExchange(secondCurrency, firstCurrency, value)
-      .then((data) => {
-        const result = data.result;
-        setFirstInputValue(result.toFixed(2));
-      })
-      .catch((error) => console.log("error", error));
+    timeout = setTimeout(() => {
+      convertExchange(secondCurrency, firstCurrency, value)
+        .then((data) => {
+          const result = data.result;
+          setFirstInputValue(result.toFixed(2));
+        })
+        .catch((error) => console.log("error", error));
+    }, 500);
   };
 
   const handleSecondSelectChange = (e) => {
-    const { value } = e.target
-    setSecondCurrency(value)
-
+    const { value } = e.target;
+    setSecondCurrency(value);
     convertExchange(firstCurrency, value, firstInputValue)
       .then((data) => {
         const result = data.result;
         setSecondInputValue(result.toFixed(2));
       })
       .catch((error) => console.log("error", error));
-  }
+  };
 
   const renderSelectOptions = () => {
-    return Object.keys(currencyRate).map(keyName => (
+    return Object.keys(currencyRate).map((keyName) => (
       <option key={keyName}>{keyName}</option>
-    ))
-  }
+    ));
+  };
 
   return (
     <div className="Convertation container">
-        <h2><u>Конвертер валют</u></h2>
-        <span>Источник: по среднему банковскому курсу</span>
+      <h2>
+        <u>Конвертер валют</u>
+      </h2>
+      <span>Источник: по среднему банковскому курсу</span>
       <div className="container input-convert">
         <div className="input-block">
           <input
@@ -93,21 +89,20 @@ function Convertation({ currencyRate }) {
           </select>
         </div>
         <div className="input-block">
-        <input
-          type="number"
-          name="input-second-currency"
-          placeholder="Введите вторую валюту"
-          value={secondInputValue}
-          onChange={handleSecondInputValue}
-        />
-        <select
-          name="select-second-currency"
-          onChange={handleSecondSelectChange}
-        >
-          {renderSelectOptions()}
-        </select>
+          <input
+            type="number"
+            name="input-second-currency"
+            placeholder="Введите вторую валюту"
+            value={secondInputValue}
+            onChange={handleSecondInputValue}
+          />
+          <select
+            name="select-second-currency"
+            onChange={handleSecondSelectChange}
+          >
+            {renderSelectOptions()}
+          </select>
         </div>
-       
       </div>
     </div>
   );
